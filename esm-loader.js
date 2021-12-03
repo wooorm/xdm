f2281ed8ceab50c5001d442bf3bbec8c88fbc08d
Titus Wormer <tituswormer@gmail.com>
2021-12-03 12:59:33 +0100

    Update dev-dependencies

--- esm-loader.js
+++ esm-loader.js
@@ -2,4 +2,5 @@ import {createLoader} from './lib/integration/node.js'
 
 const {load, getFormat, transformSource} = createLoader()
 
-export {load, getFormat, transformSource, createLoader}
+export {load, getFormat, transformSource}
+export {createLoader} from './lib/integration/node.js'
